112e0beabf8ab720b49b5a5e7caa236b529b0b44
#51978077 <itachiut1r4@gmail.com>
2020-11-11 14:24:06 -0300

    user middleware done

--- backend/src/middlewares/user.ts
+++ backend/src/middlewares/user.ts
@@ -1,50 +1,36 @@
 
 import { Request, Response, NextFunction } from 'express';
+import * as yup from 'yup' 
+//import { yupResolver } from '@hookform/resolvers/yup';
 
 import { IUser } from '../models/user';
 import response from '../common/response';
 
 
 export = {
-    valid_user(req: Request, res: Response, next: NextFunction){         
+    async valid_user(req: Request, res: Response, next: NextFunction){         
                        
-        const { email, password }: IUser = req.body;      
-     
+        const { email, password }: IUser = req.body;           
+        const yupObject = yup.object().shape({
+            email: yup.string().email().required(),
+            password: yup.string()
+                .min(8, 'Password is too short - should be 8 chars minimum.')
+                .matches( /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+                        'the password must contain at least 1 number, at least 1 lower case letter, at least 1 upper case and at least 1 special character.')
+                .required(),
+        });
 
-        if(!email){
+
+        try{
+            const validatedBody = await yupObject.validate({email, password})           
+            req.body = validatedBody;
+            
+            next();
+        }catch(err: any){
+            
             return res.json(        
-                response.jsonBadRequest(null, "Email is missing.", null)              
+                response.jsonBadRequest(null, "You didn't give us what we want!", err.message)              
             );   
-
-        }
-        
-        if(!password){
-            return res.json(        
-                response.jsonBadRequest(null, "Password is missing.", null)              
-            );   
-        }           
-
-        let emailTest = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-        let passwordTest = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-              
-        
-        if (email.match(emailTest)){
-            if (password.match(passwordTest))                                                     
-                next();
-                
-            else {
-                return res.json(        
-                    response.jsonBadRequest(null, "Invalid password.", null)              
-                );   
-            }
-        }         
-                                                        
-        else{
-            return res.json(        
-                response.jsonBadRequest(null, "Invalid email.", null)              
-            );    
-        
-        }                   
+        }     
     }  
 }
